1de80b0a9180685c0675178ca21878c940c0b499
Mary Lark <mary@marylark.com>
2018-08-31 02:02:28 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,21 @@
+def valid_move? board, index
+  #returns true for valid position on an empty board
+  #returns true for a valid position on a non-empty board
+  if !index.between?(0,8)
+      puts "Index out of range:  #{index}"
+      return false
+  elsif (board[index] == " " || board[index] == "" || board[index] == nil)
+    print "board[#{index}] = "
+    puts ' " " '
+    return true
+  else
+    return false
+  end
+end
+  #returns nil or false for an occupied position
+  #returns nil or false for a position that is not on the board
+
+
 # code your #valid_move? method here
 
 
