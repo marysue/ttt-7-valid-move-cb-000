5a557fa83324703a50f6c71b34eb039cef7d574a
Mary Lark <mary@marylark.com>
2018-08-31 01:46:32 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,19 @@
+
+def valid_move?(board, index)
+  #returns true for valid position on an empty board
+  #returns true for a valid position on a non-empty board
+  if (!index.between (1,9))
+      return false
+  elsif (board[index] == " " || board[index] == "" || board[index] == nil)
+    return true
+  else
+    return false
+  end
+end
+  #returns nil or false for an occupied position
+  #returns nil or false for a position that is not on the board
+  
+end
 # code your #valid_move? method here
 
 
